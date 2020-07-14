17bbd5de37ed43c42569f7167645cee8e470b3a0
dolev146 <dolev146@users.noreply.github.com>
2020-07-13 22:43:49 -0700

    homepage

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,24 @@
 // import Ajax from './routes.js'
 function home(outlet: HTMLElement | null) {
     if (outlet) {
-        outlet.innerHTML = "<h1>HOME</h1>"
+        let homediv = document.createElement("div")
+        outlet.appendChild(homediv)
+
+        homediv.innerHTML ="<div><h1 style=text-align:center>HOME</h1></div>"
         loadContent('./components/home/home.component.html', outlet)
 
+        
         const xhr = new XMLHttpRequest();
         xhr.open('GET', 'https://api.coingecko.com/api/v3/coins/list')
         xhr.onload = function () {
+            let containerInfo = document.createElement("div");
+            containerInfo.classList = "containerCoins"
+            outlet.appendChild(containerInfo)
+
             const data = JSON.parse(xhr.responseText);
             for (let i = 0; i < 100; i++) {
-                console.log(data[i])
                 let newdiv = document.createElement('div')
+                newdiv.classList = "Coininside"
                 let toggle_btn = document.createElement('input')
                 let infoLink = document.createElement('a')
                 infoLink.setAttribute('href', 'https://api.coingecko.com/api/v3/coins/list')
@@ -20,7 +28,7 @@ function home(outlet: HTMLElement | null) {
                 newdiv.classList.add("div_style")
                 newdiv.innerHTML += `<span>${(data[i].symbol.toUpperCase())}<br><br>
                 ${data[i].name} </span><br>`
-                outlet.appendChild(newdiv)
+                containerInfo.appendChild(newdiv)
                 newdiv.appendChild(toggle_btn)
                 newdiv.appendChild(infoLink)
                 toggle_btn.onclick = function (event) {
